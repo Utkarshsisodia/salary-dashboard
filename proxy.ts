1d988246bb661ae72bb42e8b13b578039534633e
Utkarsh Sisoida <usisodia67@gmail.com>
2026-05-06 11:05:43 +0530

    change middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   // better-auth uses these cookie names by default (secure in prod, standard in dev)
